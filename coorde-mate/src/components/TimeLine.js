6ec6c10ef84b0c6e1c11618465efc2289ce59439
kon-chan <k.chanworks@gmail.com>
2020-01-06 13:31:20 +0900

    run TimeLine component of react

---
+++ coorde-mate/src/components/TimeLine.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TimeLine extends React.Component {
+  /*
+  constructor(){
+    super();
+    this.state = {
+      datas:[]
+    };
+  }
+  */
+
+  render() {
+    return (
+      <h1>THIS TimeLine</h1>
+    );
+  }
+}
+
+export default TimeLine;
