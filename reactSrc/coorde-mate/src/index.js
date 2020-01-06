89968aab2e5aeab2a26d987cd79c545d481b1bb9
kon-chan <k.chanworks@gmail.com>
2020-01-06 16:54:43 +0900

    deleted reactSrc directory

--- reactSrc/coorde-mate/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//myComponents
-import TimeLine from './components/TimeLine';
-
-
-ReactDOM.render(<TimeLine />, document.getElementById('root'));
