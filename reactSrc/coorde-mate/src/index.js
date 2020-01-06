c83bed9e3f2fc2dba62009aae7fae9dfd6f2c166
kon-chan <k.chanworks@gmail.com>
2020-01-06 15:46:19 +0900

    TimeLine component is updated

---
+++ reactSrc/coorde-mate/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//myComponents
+import TimeLine from './components/TimeLine';
+
+
+ReactDOM.render(<TimeLine />, document.getElementById('root'));
