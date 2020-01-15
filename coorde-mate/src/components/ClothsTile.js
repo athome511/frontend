92b0889dbde87d22b500614a78b14fc82c6ebd47
kon-chan <k.chanworks@gmail.com>
2020-01-15 15:33:35 +0900

    ClothTileComponent is uncompleted

--- coorde-mate/src/components/ClothsTile.js
+++ coorde-mate/src/components/ClothsTile.js
@@ -11,8 +11,8 @@ import Loading from '../public/Loading';
 import '../css/clothsTile.css';
 
 class ClothsTile extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
       datas: []
     };
@@ -20,23 +20,41 @@ class ClothsTile extends React.Component {
 
 
   componentDidMount(){
-    console.log(this.props.userId)
     const request = axios.create({
       baseURL: 'http://18.178.35.28:3001'
     })
 
     //request.get(`/cloths/${this.props.userId}`)
-    request.get(`/cloths/${this.props.userId}`)
+    request.get(`/cloths/1`)
     .then(res => {
       this.setState({
-        datas: res.data.cloths
+        datas: res.data
       });
     })
   }
 
+/*
+  getUrl(hoge) {
+    const request = axios.create({
+      baseURL: 'http://18.178.35.28:3001'
+    })
+
+    //request.get(`/cloths/${this.props.userId}`)
+    request.get(`/cloths/${hoge}`)
+    .then(res => {
+      this.setState({
+        datas: res.data
+      });
+      //console.log(res)
+    })
+  }*/
+
+
 
   render() {
     if(!Number.isInteger(this.props.userId))  return <Loading />
+
+
     return (
       <div className="root">
       <GridList cellHeight={200} className="gridList">
@@ -44,7 +62,7 @@ class ClothsTile extends React.Component {
         <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
           <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
         </GridListTile>
-        
+
         {this.state.datas.map(tile => (
           <GridListTile key={tile.c_link} style={{width: '20%'}}>
             <img src={tile.c_link} alt={`服画像id : ${tile.c_u_id}`} />
