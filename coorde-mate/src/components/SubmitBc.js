0596bf16aa0c5ec7ca87809705e2b79c61aa4aef
kon-chan <k.chanworks@gmail.com>
2020-01-29 02:24:35 +0900

    completed submit BC

--- coorde-mate/src/components/SubmitBc.js
+++ coorde-mate/src/components/SubmitBc.js
@@ -1,60 +1,87 @@
 import React from 'react';
 import axios from 'axios';
 
+import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import ListSubheader from '@material-ui/core/ListSubheader';
 
-import Loading from '../public/Loading';
+//import Loading from '../public/Loading';
 
 
 class SubmitBc extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      datas:[],
-      flag: 0
-    };
+      proposalDatas:[]
+    }
+    this.handleClickSubmit = this.handleClickSubmit.bind(this)
+  }
+
+
+  componentWillMount(){
+    const request = axios.create({
+      baseURL: 'http://18.178.35.28:3001'
+    })
+
+    request.get(`/proposals/${this.props.match.params.requestId}`)
+    .then(res => {
+      this.setState({ proposalDatas: res.data })
+    })
+
+  }
+
+  handleClickSubmit(e) {
+    console.log(e.currentTarget.name)
+    const rootURL = 'http://18.178.35.28:3001'
+    axios.put(`${rootURL}/choose_bc/${e.currentTarget.name}`)
+    this.props.history.push('/completed')
+
+
   }
 
 
   render() {
-    const requestState = JSON.parse(localStorage.getItem('requestData'))
-    const clothsState = JSON.parse(localStorage.getItem('clothsData'))
-    //if(!Number.isInteger(1000))  return <Loading />
-    if(clothsState.c_u_id !== requestState.r_u_id) return <Loading />
-
-
-    if(this.state.flag === 0) {
-      const request = axios.create({
-        baseURL: 'http://18.178.35.28:3001/'
-      })
-      request.get(`/cloths/${requestState.r_u_id}`)
-      .then(res => {
-        this.setState({
-          datas: res.data.c_link_data,
-          flag: 1
-        });
-      })
-    }
-
     return (
-      <div className="root">
-        <GridList cellHeight={200} className="gridList">
-          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
-          </GridListTile>
-
-          {/* 所有服一覧表示 */}
-          {this.state.datas.map((cloth) => {
+      <React.Fragment>
+        {this.state.proposalDatas.map((cloth) => {
+          if(cloth.id) {
             return (
-              <GridListTile key={cloth.id} style={{width: '20%'}}>
-                <img src={cloth.c_link} alt={`服画像id : ${cloth.c_u_id}`} />
-              </GridListTile>
+              <React.Fragment>
+              <div className="root">
+                <GridList cellHeight={200} className="gridList">
+                  <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
+                  </GridListTile>
+
+                  {/* 提案服一覧表示 */}
+                  {cloth.p_selected_data.map((cloth) => {
+                    return (
+                      <GridListTile key={cloth.id} style={{width: '20%'}}>
+                        <img src={cloth.link} alt={`服画像id : ${cloth.id}`} />
+                      </GridListTile>
+                    )
+                  })}
+
+
+                  <p>memo: {cloth.p_memo}</p>
+
+                </GridList>
+              </div>
+              <Button
+                onClick = {(e) => this.handleClickSubmit(e)}
+                name = {cloth.id}
+                variant = "contained"
+                >
+                この提案をBCにする
+              </Button>
+            </React.Fragment>
+
             )
-          })}
-        </GridList>
-      </div>
+          }
+        })}
+
+      </React.Fragment>
     );
   }
 }
