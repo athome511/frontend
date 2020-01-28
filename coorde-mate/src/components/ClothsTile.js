fc018278f3936c5d0b492558a518ed560818f889
kon-chan <k.chanworks@gmail.com>
2020-01-29 00:17:47 +0900

    all output proposal Cloth

--- coorde-mate/src/components/ClothsTile.js
+++ coorde-mate/src/components/ClothsTile.js
@@ -6,8 +6,8 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import ListSubheader from '@material-ui/core/ListSubheader';
 
-import Loading from '../public/Loading';
 
+import Loading from '../public/Loading';
 //css
 import '../css/clothsTile.css';
 
@@ -17,55 +17,50 @@ class ClothsTile extends React.Component {
   constructor(){
     super();
     this.state = {
-      datas:[]
+      datas:[],
+      flag: 0
     };
   }
 
-  componentWillMount() {
-    const request = axios.create({
-      baseURL: 'http://18.178.35.28:3001'
-    })
-    console.log(this.props)
-
-    request.get(`/cloths/${this.props.userId}`)
-    .then(res => {
-      this.setState({ datas: res.data })
-    })
-  }
-
-
   render() {
-    while(this.props.userId === undefined) {
-      return <Loading />
+    const requestState = JSON.parse(localStorage.getItem('requestData'))
+    const clothsState = JSON.parse(localStorage.getItem('clothsData'))
+    //if(!Number.isInteger(1000))  return <Loading />
+    if(clothsState.c_u_id !== requestState.r_u_id) return <Loading />
+
+
+    if(this.state.flag === 0) {
+      const request = axios.create({
+        baseURL: 'http://18.178.35.28:3001/'
+      })
+      request.get(`/cloths/${requestState.r_u_id}`)
+      .then(res => {
+        this.setState({
+          datas: res.data.c_link_data,
+          flag: 1
+        });
+      })
     }
 
+
+
     return (
-      <React.Fragment>
-        {console.log(this.props)}
-        {this.state.datas.map((cloth) => {
-          if(cloth.id) {
+      <div className="root">
+        <GridList cellHeight={200} className="gridList">
+          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+            <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
+          </GridListTile>
+
+          {/* 所有服一覧表示 */}
+          {this.state.datas.map((cloth) => {
             return (
-              <div className="root">
-                <GridList cellHeight={200} className="gridList">
-                  <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
-                  </GridListTile>
-
-                  {/* 所有服一覧表示 */}
-                  {cloth.c_link_data.map((cloth) => {
-                    return (
-                      <GridListTile key={cloth.id} style={{width: '20%'}}>
-                        <img src={cloth.c_link} alt={`服画像id : ${cloth.id}`} />
-                      </GridListTile>
-                    )
-                  })}
-                </GridList>
-              </div>
+              <GridListTile key={cloth.id} style={{width: '20%'}}>
+                <img src={cloth.c_link} alt={`服画像id : ${cloth.c_u_id}`} />
+              </GridListTile>
             )
-          }
-        })}
-
-      </React.Fragment>
+          })}
+        </GridList>
+      </div>
     )
   }
 }
--- coorde-mate/src/components/SubmitBc.js
+++ coorde-mate/src/components/SubmitBc.js
@@ -9,52 +9,58 @@ import Loading from '../public/Loading';
 
 
 class SubmitBc extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      datas:[],
-      flag: 0
-    };
+      proposalDatas:[],
+    }
+  }
+
+
+  componentWillMount(){
+    const request = axios.create({
+      baseURL: 'http://18.178.35.28:3001'
+    })
+
+    request.get(`/proposals/${this.props.match.params.requestId}`)
+    .then(res => {
+      this.setState({ proposalDatas: res.data })
+    })
+
   }
 
 
   render() {
-    const requestState = JSON.parse(localStorage.getItem('requestData'))
-    const clothsState = JSON.parse(localStorage.getItem('clothsData'))
-    //if(!Number.isInteger(1000))  return <Loading />
-    if(clothsState.c_u_id !== requestState.r_u_id) return <Loading />
-
-
-    if(this.state.flag === 0) {
-      const request = axios.create({
-        baseURL: 'http://18.178.35.28:3001/'
-      })
-      request.get(`/cloths/${requestState.r_u_id}`)
-      .then(res => {
-        this.setState({
-          datas: res.data.c_link_data,
-          flag: 1
-        });
-      })
-    }
-
     return (
-      <div className="root">
-        <GridList cellHeight={200} className="gridList">
-          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
-          </GridListTile>
-
-          {/* 所有服一覧表示 */}
-          {this.state.datas.map((cloth) => {
+      <React.Fragment>
+        {this.state.proposalDatas.map((cloth) => {
+          if(cloth.id) {
             return (
-              <GridListTile key={cloth.id} style={{width: '20%'}}>
-                <img src={cloth.c_link} alt={`服画像id : ${cloth.c_u_id}`} />
-              </GridListTile>
+              <div className="root">
+                <GridList cellHeight={200} className="gridList">
+                  <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div" className="listSubheader" style={{fontSize: '2rem'}}>所有服一覧</ListSubheader>
+                  </GridListTile>
+
+                  {/* 提案服一覧表示 */}
+                  {cloth.p_selected_data.map((cloth) => {
+                    return (
+                      <GridListTile key={cloth.id} style={{width: '20%'}}>
+                        <img src={cloth.link} alt={`服画像id : ${cloth.id}`} />
+                      </GridListTile>
+                    )
+                  })}
+
+
+                  <p>memo: {cloth.p_memo}</p>
+
+                </GridList>
+              </div>
             )
-          })}
-        </GridList>
-      </div>
+          }
+        })}
+
+      </React.Fragment>
     );
   }
 }
