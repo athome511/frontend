04a84b9c43bf6bdba2371745b3bd679985cc1fb5
TaigaIwamoto <210291e@ugs.kochi-tech.ac.jp>
2020-01-30 06:08:54 +0900

    fixed mypage css

--- coorde-mate/src/components/MyPageCloset.js
+++ coorde-mate/src/components/MyPageCloset.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import FileInputComponent from 'react-file-input-previews-base64';
 
 import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
@@ -10,6 +11,30 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 
 
 class MyPageCloset extends React.Component {
+
+    constructor(){
+	super();
+	this.state = {
+	    cloth: '',
+	    flag: true
+	}
+    }
+
+    hundleSubmit = () => {
+	const userState = JSON.parse(localStorage.getItem('userData'))
+	this.createCloth(userState.id, this.state.cloth)
+	this.setState({cloth:''})
+    }
+
+    createCloth = (id,cloth) => {
+	axios.post(`http://18.178.35.28:3001/cloths`,{c_u_id: id, c_link: cloth})
+	    .then((response) => {
+		console.log(response)
+	    }
+		 )
+    }
+    
+    
   componentWillMount() {
     const userState = JSON.parse(localStorage.getItem('userData'))
 
@@ -21,17 +46,14 @@ class MyPageCloset extends React.Component {
       localStorage.setItem('clothsData', JSON.stringify(res.data))
     })
 
-  }
+  }    
 
 
-
-  render() {
-    const clothsState = JSON.parse(localStorage.getItem('clothsData'))
-
-
-
+    render() {
+	const clothsState = JSON.parse(localStorage.getItem('clothsData'))
+	
     return (
-      <React.Fragment>
+	    <React.Fragment>
         <div className="root">
           <GridList cellHeight={200} className="gridList">
             <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
@@ -39,29 +61,36 @@ class MyPageCloset extends React.Component {
             </GridListTile>
 
             {/* 所有服一覧表示 */}
-
             {clothsState.c_link_data.map((cloth) => {
               return (
                 <GridListTile key={cloth.id} style={{width: '20%'}}>
                   <img src={cloth.c_link} alt={`服画像id : ${cloth.c_u_id}`} />
                 </GridListTile>
               )
-            })}
+            })
+	    }
 
           </GridList>
-        </div>
-
-        <input
-          accept="image/*"
-          id="contained-button-file"
-          multiple
-          type="file"
-          />
-        <label htmlFor="contained-button-file">
-          <Button variant="contained" color="primary" component="span">
-            Upload
-          </Button>
-        </label>
+            </div>
+	    <div className="changeIcon">
+	    <div>
+	    <FileInputComponent
+              labelText = "cloth"
+              parentStyle={{}} //スタイル
+              imagePreview={true} //ファイルのプレビュー
+              multiple={false} //複数ファイル選択
+              callbackFunction={file => { //選択後のコールバック関数
+              console.log(file)
+              this.setState({cloth: file.base64})
+            }}
+            buttonComponent={ //クリック時に選択ダイアログを開くコンポーネント
+              <button type="chooseIcon">ファイルを選択</button>
+            }
+            accept="image/*" //許可するファイルのtype
+            />
+	    </div>
+	    <button type="submit" onClick={() => {this.hundleSubmit()}}>追加</button>
+	    </div>
 
       </React.Fragment>
     );
